0c0a10d61f33b50315946bcff8bbb9d732f48907
yusaku.cho <2023dounyukenshu.064@gmail.com>
2023-05-18 13:14:10 +0900

    フラッシュメッセージを追加

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -32,27 +32,34 @@ public class UpdateServlet extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _token = request.getParameter("_token");
-        if(_token !=null && _token.equals(request.getSession().getId())) {
+        if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
             Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
 
+            // フォームの内容を各フィールドに上書き
             String title = request.getParameter("title");
             m.setTitle(title);
 
-            String content =request.getParameter("contetnt");
+            String content = request.getParameter("content");
             m.setContent(content);
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setUpdated_at(currentTime);
+            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
 
+            // データベースを更新
             em.getTransaction().begin();
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "更新が完了しました。");
             em.close();
 
+            // セッションスコープ上の不要になったデータを削除
             request.getSession().removeAttribute("message_id");
+
+            // indexページへリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
         }
     }
-
 }
