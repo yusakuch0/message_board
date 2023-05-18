4fc21903e902a5fbe2f8dee11b0e241969be314f
yusaku.cho <2023dounyukenshu.064@gmail.com>
2023-05-18 11:55:59 +0900

    stylesheetsを適用

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -44,10 +44,11 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
 
         // メッセージIDをセッションスコープに登録
+        if(m != null) {
         request.getSession().setAttribute("message_id", m.getId());
-
+        }
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
-    }
 
+    }
 }
